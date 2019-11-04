093b5fae4a8c8000f192273b4347134df7996b5b
Jon Carlo Vargas <jcjv94@gmail.com>
2019-11-04 11:54:39 -0800

    connect spin to button

--- js/main.js
+++ js/main.js
@@ -1,39 +1,64 @@
 // create slots- slot1, slot2, slot3
-const seven = new Image();
-const lemon = new Image();
-const cherry = new Image();
-const orange = new Image();
+// const seven = img();
+// const lemon = img();
+// const cherry = img();
+// const orange = img();
 
-seven.src = "Images/Slot image/seven.png";
-lemon.src = "Images/Slot image/lemon.png";
-cherry.src = "Images/Slot image/cherry.png";
-orange.src = "Images/Slot image/orange.png";
-
-
-
-let slot1 = [seven, lemon, cherry, orange]
-let slot2 = [seven, lemon, cherry, orange]]◊
-let slot3 = [seven, lemon, cherry, orange]]
+// seven.src = "https://raw.githubusercontent.com/jcjv94/Slot-Machine/master/Images/Slot%20image/seven.png";
+// lemon.src = "https://raw.githubusercontent.com/jcjv94/Slot-Machine/master/Images/Slot%20image/lemon.png";
+// cherry.src = "https://raw.githubusercontent.com/jcjv94/Slot-Machine/master/Images/Slot%20image/cherry.png";
+// orange.src = "https://raw.githubusercontent.com/jcjv94/Slot-Machine/master/Images/Slot%20image/orange.png";
 
 
-// randomize slot1
-let showSlot1 = slot1[Math.floor(Math.random() * slot1.length)];
-// console.log(showSlot1)
 
-document.getElementById('slt1').innerText = showSlot1
-
-// randomize slot 2
-let showSlot2 = slot2[Math.floor(Math.random() * slot2.length)];
-// console.log(showSlot2)
-document.getElementById('slt2').innerText = showSlot2
-
-// randomize slot3
-let showSlot3 = slot3[Math.floor(Math.random() * slot3.length)];
-// console.log(showSlot3)
-document.getElementById('slt3').innerText = showSlot3
+// let slot1 = [seven, lemon, cherry, orange]
+// let slot2 = [seven, lemon, cherry, orange]
+// let slot3 = [seven, lemon, cherry, orange]
 
 
-// document.querySelectorAll('button').addEventListener('click', init);
+
+let slot1 = ["Seven", "Lemon", "Cherry", "Orange"]
+let slot2 = ["Seven", "Lemon", "Cherry", "Orange"]
+let slot3 = ["Seven", "Lemon", "Cherry", "Orange"]
+
+let showSlot1, showSlot2, showSlot3;
+
+document.querySelector('button').addEventListener('click', spin);
+
+
+function spin() {
+    // randomize slot1
+    showSlot1 = slot1[Math.floor(Math.random() * slot1.length)];
+    // console.log(showSlot1)
+
+    document.getElementById('slt1').innerText = showSlot1
+
+    // randomize slot 2
+    showSlot2 = slot2[Math.floor(Math.random() * slot2.length)];
+    // console.log(showSlot2)
+    document.getElementById('slt2').innerText = showSlot2
+
+    // randomize slot3
+    showSlot3 = slot3[Math.floor(Math.random() * slot3.length)];
+    // console.log(showSlot3)
+    document.getElementById('slt3').innerText = showSlot3
+
+    winner();
+}
+
+let results = document.getElementById('result');
+
+function winner() {
+    if ((showSlot1 === showSlot2) && (showSlot2 === showSlot3) && (showSlot3 === showSlot1)) {
+        results.textContent = "You Win!";
+    } else {
+        results.textContent = "You Lose";
+    }
+}
+
+
+
+// document.querySelector('button').addEventListener('click', spin)
 // document.querySelectorAll('.allSlots').addEventListener('click', spin);
 
 
@@ -43,19 +68,8 @@ document.getElementById('slt3').innerText = showSlot3
 //     render();
 //   }
 
-  
 
-let results = document.getElementById('result');
 
-function winner() {
-    if ((showSlot1 === showSlot2) && (showSlot2 === showSlot3) && (showSlot3 === showSlot1)) {
-        results.textContent = "You Win!";
-    } else {
-        results.textContent = "You Lose";
-        }
-}
-
-winner();
 
 // initialize null- empty slots, no one has pushed the button (or lever) 
 
